28c9a680eb001c4243723eb8fdbef1dae62b79e4
dannto <donhattan1012dev@gmail.com>
2023-11-25 15:37:53 +0700

    reformat code, add module's docstring

--- Consumer.py
+++ Consumer.py
@@ -1,43 +1,43 @@
 import argparse
+from io import BytesIO
 from flask import Flask, Response, render_template
 from kafka import KafkaConsumer
 from PIL import Image, ImageDraw
-from io import BytesIO
 import torch
-import torchvision.transforms as transforms
 
 import findspark
-import pyspark
 from pyspark.sql import SparkSession
 
 def parse_args():
+    """
+    Parse User's input arguments.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--bootstrap_server", 
-                        type=str, 
+    parser.add_argument("-b", "--bootstrap_server",
+                        type=str,
                         default="localhost:9092",
                         help="Kafka Bootstrap Server")
-    parser.add_argument("-t0", "--topic_0", 
-                        type=str, 
+    parser.add_argument("-t0", "--topic_0",
+                        type=str,
                         default="topic_camera_00",
                         help="The Topic Name for Camera 0.")
-    parser.add_argument("-t1", "--topic_1", 
-                        type=str, 
+    parser.add_argument("-t1", "--topic_1",
+                        type=str,
                         default="topic_camera_01",
                         help="The Topic Name for Camera 1.")
-    parser.add_argument("-p", "--port", 
+    parser.add_argument("-p", "--port",
                         type=int, 
                         default=5000,
                         help="Port to run the Application.")
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
-# 
+# User input arguments Constants
 args    = vars(parse_args())
 BOOSTRAP_SERVER = args['bootstrap_server']
 TOPIC_0 = args['topic_0']
 TOPIC_1 = args['topic_1']
 PORT    = args['port']
-
+# Other setup Constants
 SCALA_VERSION = '2.12'
 SPARK_VERSION = '3.5.0'
 KAFKA_VERSION = '3.6.0'
@@ -46,7 +46,6 @@ packages = [
     f'org.apache.spark:spark-sql-kafka-0-10_{SCALA_VERSION}:{SPARK_VERSION}',
     f'org.apache.kafka:kafka-clients:{KAFKA_VERSION}'
 ]
-    
 # Fire up Spark
 findspark.init()
 spark = SparkSession.builder \
@@ -62,7 +61,7 @@ yolo_detector.classes = [0] # Only detect "person" (Class index 0 in COCO Datase
 
 # Fire up the Kafka Consumers
 consumer1 = KafkaConsumer(
-    TOPIC_0, 
+    TOPIC_0,
     bootstrap_servers=['localhost:9092'])
 
 consumer2 = KafkaConsumer(
@@ -72,21 +71,42 @@ consumer2 = KafkaConsumer(
 app = Flask(__name__)
 @app.route('/')
 def index():
+    """
+    Main Route that displays the Dashboard for our Cameras.
+    """
     return render_template('index.html')
 
 @app.route('/camera1', methods=['GET'])
 def camera1():
+    """
+    Route that contains the data from Camera 1.
+    """
     return Response(
         get_video_stream(consumer1), 
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/camera2', methods=['GET'])
 def camera2():
+    """
+    Route that contains the data from Camera 2.
+    """
     return Response(
         get_video_stream(consumer2), 
         mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def detect_human(input_image):
+    """
+    Return Images with detected boxes for each person.
+
+    Parameters:
+    ----------
+    input_image: byte,
+            an Image stored in `bytes` type that need to be detected.
+
+    Returns
+    ----------
+    The result Image after drawing detected boxes, stored in `bytes`.
+    """
     # Convert the received image bytes to a PIL Image
     image = Image.open(BytesIO(input_image))
 
@@ -100,12 +120,11 @@ def detect_human(input_image):
 
     # Draw bounding boxes around detected objects
     for detection in results.xyxy[0]:
-        # if int(detection[-1]) == 0:  # Check if the detected object is a person (class 0 for COCO dataset)
-            xmin, ymin, xmax, ymax = map(int, detection[:4])
-            label = f"{yolo_detector.names[int(detection[-1])]} {detection[-2]:.2f}"
-            draw = ImageDraw.Draw(image)
-            draw.rectangle([xmin, ymin, xmax, ymax], outline="red", width=2)
-            draw.text((xmin, ymin), label, fill="red")
+        xmin, ymin, xmax, ymax = map(int, detection[:4])
+        label = f"{yolo_detector.names[int(detection[-1])]} {detection[-2]:.2f}"
+        draw = ImageDraw.Draw(image)
+        draw.rectangle([xmin, ymin, xmax, ymax], outline="red", width=2)
+        draw.text((xmin, ymin), label, fill="red")
 
     # Convert the modified image back to bytes
     buffered = BytesIO()
