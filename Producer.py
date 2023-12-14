dd9a8b2324438dd6da3014da2cdf876dad62045e
dannto <donhattan1012dev@gmail.com>
2023-12-14 15:04:13 +0700

    produce folder of images from Producer

--- Producer.py
+++ Producer.py
@@ -1,7 +1,10 @@
+import os
 import argparse
 import time
 import cv2
 from kafka import KafkaProducer
+
+INTERVAL = 0.2
 
 
 def parse_args():
@@ -19,41 +22,65 @@ def parse_args():
     return args
 
 
-def publish_video(video_file, topic):
+def publish_video(video_path: str, topic):
     """
     Publish given video file to a specified Kafka topic. 
+    There are 2 possible `video_path` input, a link to a video (end with ".mp4")
+    or a path to a folder full of images.
+
     Kafka Server is expected to be running on the localhost. Not partitioned.
 
+    Parameters
     ----------
-    Parameters:
-    - `video_file`: str, path to video file (camera demo)
-    - `topic`: str, the topic to be published to.
+    `video_path`: str
+        path to video file (camera demo)
+    `topic`: str
+        the topic to be published to.
     """
+
+    def encode_and_produce(frame, producer: KafkaProducer, interval: int):
+        # Convert image to jpg format
+        ret, buffer = cv2.imencode('.jpg', frame)
+
+        # Convert to bytes and send to Kafka
+        producer.send(topic, buffer.tobytes())
+
+        time.sleep(interval)
 
     # Start up producer
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-    # Open file
-    video = cv2.VideoCapture(video_file)
+    if '.mp4' in video_path:
+        # Open file
+        video = cv2.VideoCapture(video_path)
+        print('publishing video...')
 
-    print('publishing video...')
+        while (video.isOpened()):
+            success, frame = video.read()
 
-    while (video.isOpened()):
-        success, frame = video.read()
+            # Ensure file was read successfully
+            if not success:
+                print("bad read!")
+                break
+            encode_and_produce(
+                frame=frame, producer=producer, interval=INTERVAL)
+        video.release()
+    else:
+        # Open folder
+        image_files = [f for f in os.listdir(video_path)
+                       if f.endswith(('.jpg', '.png'))]
+        print('publishing video...')
 
-        # Ensure file was read successfully
-        if not success:
-            print("bad read!")
-            break
+        for img in image_files:
+            image_path = os.path.join(video_path, img)
+            frame = cv2.imread(image_path)
 
-        # Convert image to png
-        ret, buffer = cv2.imencode('.jpg', frame)
+            encode_and_produce(
+                frame=frame,
+                producer=producer,
+                interval=INTERVAL
+            )
 
-        # Convert to bytes and send to kafka
-        producer.send(topic, buffer.tobytes())
-
-        time.sleep(0.05)
-    video.release()
     print('publish complete')
 
 
@@ -66,7 +93,7 @@ def main():
     camera = args['camera']
     topic = args['topic']
 
-    publish_video(video_file=camera, topic=topic)
+    publish_video(video_path=camera, topic=topic)
 
 
 if __name__ == '__main__':
