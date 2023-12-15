79e46b2f1471fdaf055c37c6bc2e9132b8276015
dannto <donhattan1012dev@gmail.com>
2023-12-15 07:00:19 +0700

    improve person_detector fnc, fix classifier docstring

--- re_id/classifier.py
+++ re_id/classifier.py
@@ -10,20 +10,28 @@ class PersonReID():
         """Init a Classifier"""
         self.gallery = self.init_gallery(from_scratch, custom_tensor_path)
 
-    def init_gallery(self, from_scratch: bool = False, custom_tensor_path: str = None) -> torch.Tensor:
+    def init_gallery(
+        self,
+        from_scratch: bool = False,
+        from_file: str = None,
+        from_tensor: torch.Tensor = None
+    ) -> torch.Tensor:
         """
         Init the gallery from reading a torch.tensor saved in a `.pt` file or init a new torch.tensor.
+        There are 3 options to init a gallery, which represented by the 3 parameters.
+        These path SHOULDN'T be set add the same time.
 
         Parameters
         ----------
         from_scratch: bool, default False
             Wheater to to init the tensor from scratch (create an empty tensor).
 
+        from_file: str, default None
+            A path, a custom saved tensor saved on the disk.
+            If this is set, load the saved tensor in path to be gallery.
 
-        custom_tensor_path: str, default None
-            Use a custom saved tensor. 
-            If leaving it as default, init a new tensor if from_scratch=True,
-            else read the saved tensor from SAVED_PATH
+        from_tensor: torch.Tensor, default None
+            A tensor. It this is set, load the given tensor to be gallery.
 
         Returns
         -------
--- realtime_reid/person_detector.py
+++ realtime_reid/person_detector.py
@@ -1,6 +1,6 @@
-import torch
 from io import BytesIO
-from PIL import Image, ImageDraw
+from PIL import Image
+import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -17,14 +17,16 @@ class PersonDetector():
         # Only detect "person" (Class index 0 in COCO Dataset)
         self.yolo.classes = [0]
 
-    def detect_human(self, input_image):
+    def detect_human(self, input_image: bytes, save_detection: bool = False):
         """
         Return Images with detected boxes for each person.
 
         Parameters
         ----------
-        input_image: byte
+        input_image: bytes
             an Image stored in `bytes` type that need to be detected.
+        save_detection: bool, defaut False
+            wheater to save image and detected people into disk.
 
         Returns
         -------
@@ -37,20 +39,38 @@ class PersonDetector():
         results = self.yolo([image])
 
         # Crop & Save detected people for Re-ID task
-        results.crop(save=True)
-
-        results.render()
-
-        # Draw bounding boxes around detected objects
-        for detection in results.xyxy[0]:
-            xmin, ymin, xmax, ymax = map(int, detection[:4])
-            label = f"{self.yolo.names[int(detection[-1])]} {detection[-2]:.2f}"
-            draw = ImageDraw.Draw(image)
-            draw.rectangle([xmin, ymin, xmax, ymax], outline="red", width=2)
-            draw.text((xmin, ymin), label, fill="red")
+        results.crop(save=save_detection)
+        drawed_image = Image.fromarray(results.render()[0])
 
         # Convert the modified image back to bytes
         buffered = BytesIO()
-        image.save(buffered, format="JPEG")
+        drawed_image.save(buffered, format="JPEG")
 
         return buffered
+
+    def detect_complex(self, input_bytes: bytes):
+        """
+        Return (alot of) data store of a detected image, save in `dict`.
+
+        Parameters
+        ----------
+        input_bytes: byte
+            an Image stored in `bytes` type that need to be detected.
+
+        Returns
+        -------
+        The result Image after drawing detected boxes, stored in `bytes`.
+        """
+        # Convert the received image bytes to a PIL Image
+        image = Image.open(BytesIO(input_bytes))
+
+        # Perform YOLOv5 object detection
+        results = self.yolo([image])
+
+        # Crop & Save detected people for Re-ID task
+        detected_ppl = results.crop(save=True)
+
+        return {
+            'result': results,
+            'detected_ppl': detected_ppl,
+        }
