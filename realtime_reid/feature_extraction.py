85e8fd14b366e7cc6291e0766cf20cedd5dd98fd
dannto <donhattan1012dev@gmail.com>
2023-12-15 07:30:50 +0700

    major feature_extraction fix, move it to realtime_reid module

--- realtime_reid/feature_extraction.py
+++ realtime_reid/feature_extraction.py
@@ -1,13 +1,12 @@
 import os
-import PIL
+from PIL import Image
 import torch
-import torch.nn as nn
+from torch import nn
 from torch.autograd import Variable
-from torchvision import datasets, models, transforms
+from torchvision import transforms
 from tqdm import tqdm
-from model import ft_net
+from .resnet_base import ft_net
 
-use_gpu = torch.cuda.is_available()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 h, w = 256, 128
 # Model Parameters
@@ -29,19 +28,19 @@ class ResNetReID():
 
         # Init model structure
         self.model_structure = ft_net(
-            class_num=N_CLASSES, 
-            stride=STRIDE, 
+            class_num=N_CLASSES,
+            stride=STRIDE,
             ibn=False,
             linear_num=LINEAR_NUM,
         )
 
         # Now load model from checkpoint
         self.model = self.load_network(self.model_structure)
-        self.model.classifier.classifier = nn.Sequential() # Remove the final fc layer and classifier layer
+        # Remove the final fc layer and classifier layer
+        self.model.classifier.classifier = nn.Sequential()
 
         self.model = self.model.eval()
-        if use_gpu:
-            self.model = self.model.cuda()
+        self.model = self.model.to(device)
 
     def load_network(self, network):
         """
@@ -51,44 +50,33 @@ class ResNetReID():
         network.load_state_dict(torch.load(save_path))
         return network
 
-
     def fliplr(self, img):
         """flip horizontal"""
-        inv_idx = torch.arange(img.size(3)-1,-1,-1).long()  # N x C x H x W
-        img_flip = img.index_select(3,inv_idx)
+        inv_idx = torch.arange(img.size(3)-1, -1, -1).long()  # N x C x H x W
+        img_flip = img.index_select(3, inv_idx)
         return img_flip
 
     def extract_feature_single(self, img_path: str):
         """
         Extract feature from a trained `model`, with the input is an Image (or list of Images).
         """
-        img = PIL.Image.open(img_path)
+        img = Image.open(img_path)
         img = self.data_transforms(img)
         img = img.unsqueeze(0)
         n, c, h, w = img.size()
 
         ff = torch.FloatTensor(n, LINEAR_NUM).zero_()
-        if use_gpu:
-            ff.to(device)
+        ff = ff.to(device)
 
         for i in range(2):
-            if(i==1):
+            if (i == 1):
                 img = self.fliplr(img)
-            input_img = img
-            if (use_gpu):
-                input_img = Variable(img.cuda()) 
+            input_img = img.to(device)
 
             with torch.no_grad():
-                outputs = self.model(input_img) 
+                outputs = self.model(input_img)
             ff += outputs
         # norm feature
         fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
         ff = ff.div(fnorm.expand_as(ff))
         return ff
-
-model = ResNetReID()
-
-img_path = '../human_reidentification/runs/detect/exp/image0.jpg'
-for i in range(20):
-    test_feature = model.extract_feature_single(img_path)
-    print(test_feature, test_feature.size())
