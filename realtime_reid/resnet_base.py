1f3431b16241060b81211c256b51555907b8f7b0
dannto <donhattan1012dev@gmail.com>
2023-12-16 08:33:56 +0700

    format resnet_base, change model_path in feature_extraction

--- realtime_reid/resnet_base.py
+++ realtime_reid/resnet_base.py
@@ -6,18 +6,20 @@ import torch.nn as nn
 from torch.nn import init
 from torchvision import models
 from torch.autograd import Variable
-######################################################################
+
+
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
-    # print(classname)
     if classname.find('Conv') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in') # For old pytorch, you may use kaiming_normal.
+        # For old pytorch, you may use kaiming_normal.
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
     elif classname.find('Linear') != -1:
         init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
     elif classname.find('BatchNorm1d') != -1:
         init.normal_(m.weight.data, 1.0, 0.02)
     if hasattr(m, 'bias') and m.bias is not None:
         init.constant_(m.bias.data, 0.0)
+
 
 def weights_init_classifier(m):
     classname = m.__class__.__name__
@@ -27,12 +29,21 @@ def weights_init_classifier(m):
 
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
+
+
 class ClassBlock(nn.Module):
-    def __init__(self, input_dim, class_num, droprate, relu=False, bnorm=True, linear=512, return_f = False):
+    def __init__(self,
+                 input_dim,
+                 class_num,
+                 droprate,
+                 relu=False,
+                 bnorm=True,
+                 linear=512,
+                 return_f=False):
         super(ClassBlock, self).__init__()
         self.return_f = return_f
         add_block = []
-        if linear>0:
+        if linear > 0:
             add_block += [nn.Linear(input_dim, linear)]
         else:
             linear = input_dim
@@ -40,7 +51,7 @@ class ClassBlock(nn.Module):
             add_block += [nn.BatchNorm1d(linear)]
         if relu:
             add_block += [nn.LeakyReLU(0.1)]
-        if droprate>0:
+        if droprate > 0:
             add_block += [nn.Dropout(p=droprate)]
         add_block = nn.Sequential(*add_block)
         add_block.apply(weights_init_kaiming)
@@ -52,32 +63,47 @@ class ClassBlock(nn.Module):
 
         self.add_block = add_block
         self.classifier = classifier
+
     def forward(self, x):
         x = self.add_block(x)
         if self.return_f:
             f = x
             x = self.classifier(x)
-            return [x,f]
+            return [x, f]
         else:
             x = self.classifier(x)
             return x
 
-# Define the ResNet50-based Model
-class ft_net(nn.Module):
 
-    def __init__(self, class_num=751, droprate=0.5, stride=2, circle=False, ibn=False, linear_num=512):
+class ft_net(nn.Module):
+    """The ResNet50-based Model"""
+
+    def __init__(
+        self,
+        class_num=751, droprate=0.5, stride=2,
+        circle=False, ibn=False, linear_num=512
+    ):
         super(ft_net, self).__init__()
-        model_ft = models.resnet50(pretrained=True)
-        if ibn==True:
-            model_ft = torch.hub.load('XingangPan/IBN-Net', 'resnet50_ibn_a', pretrained=True)
+
+        resnet_weights = models.ResNet50_Weights.DEFAULT
+        model_ft = models.resnet50(resnet_weights)
+
+        if ibn is True:
+            model_ft = torch.hub.load(
+                'XingangPan/IBN-Net',
+                'resnet50_ibn_a',
+                pretrained=True
+            )
+
         # avg pooling to global pooling
         if stride == 1:
-            model_ft.layer4[0].downsample[0].stride = (1,1)
-            model_ft.layer4[0].conv2.stride = (1,1)
-        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
+            model_ft.layer4[0].downsample[0].stride = (1, 1)
+            model_ft.layer4[0].conv2.stride = (1, 1)
+        model_ft.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.model = model_ft
         self.circle = circle
-        self.classifier = ClassBlock(2048, class_num, droprate, linear=linear_num, return_f = circle)
+        self.classifier = ClassBlock(
+            2048, class_num, droprate, linear=linear_num, return_f=circle)
 
     def forward(self, x):
         x = self.model.conv1(x)
@@ -92,20 +118,3 @@ class ft_net(nn.Module):
         x = x.view(x.size(0), x.size(1))
         x = self.classifier(x)
         return x
-
-'''
-# debug model structure
-# Run this code with:
-python model.py
-'''
-if __name__ == '__main__':
-# Here I left a simple forward function.
-# Test the model, before you train it. 
-    net = ft_net_hr(751)
-    #net = ft_net_swin(751, stride=1)
-    net.classifier = nn.Sequential()
-    print(net)
-    input = Variable(torch.FloatTensor(8, 3, 224, 224))
-    output = net(input)
-    print('net output size:')
-    print(output.shape)
